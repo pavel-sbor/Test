b16fad542b25a9d29bcb4ac6bab76a0423a04f38
Pavel <pavel-sbor@rambler.ru>
2015-02-10 19:52:29 +0300

    3C

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,8 @@
-/**
- * Created by Pavel on 09.02.2015.
- */
 public class Test {
     public static void main(String[] args)
     {
         System.out.println("Hello word!");
+        System.out.println("Hello world!");
         System.out.println("Hello world!");
     }
 }
